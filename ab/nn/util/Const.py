ec524e0ad93085704394ea163de07dc8cac75c5a
ABrain-One <AI-Linux@ABrain.one>
2025-10-13 18:26:31 +0200

    Refactoring

--- ab/nn/util/Const.py
+++ ab/nn/util/Const.py
@@ -47,7 +47,8 @@ def model_script(name):
 default_nn_name = 'AlexNet'
 default_nn_path = model_script(default_nn_name)
 transform_dir = nn_path('transform')
-stat_dir = nn_path('stat/train')
+stat_dir = nn_path('stat')
+stat_train_dir = stat_dir / 'train'
 
 
 def __project_root_path():
