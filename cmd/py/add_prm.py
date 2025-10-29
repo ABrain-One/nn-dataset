1463ce2549425bab7312dfbe70f1218fdb31c1db
ABrain-One <AI-Linux@ABrain.one>
2025-10-29 11:48:31 +0100

    Directory 'cmd'

---
+++ cmd/py/add_prm.py
@@ -0,0 +1,34 @@
+import json
+import os.path
+from pathlib import Path
+
+from ab.nn.util.Util import merge_prm
+
+def gpu_type():
+    return None
+def cpu_type():
+    return None
+
+for p in Path(".").iterdir():
+    if not os.path.isdir(p):
+        continue
+    for epoch_file in p.iterdir():
+        try:
+            epoch = int(epoch_file.stem)
+        except Exception:
+            continue
+
+        with open(epoch_file, 'r') as f:
+            trials = json.load(f)
+
+        trials2 = []
+        for prm in trials:
+            prm2 = merge_prm(prm,
+                             {
+                                 'gpu': gpu_type(),
+                                 'cpu': cpu_type(),
+                             })
+            trials2.append(prm2)
+
+        with open(epoch_file, "w") as f:
+            json.dump(trials2, f, indent=4)
