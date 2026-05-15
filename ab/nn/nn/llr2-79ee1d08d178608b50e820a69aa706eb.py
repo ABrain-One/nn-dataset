9c3bd355dcfc11380f40e36bae305eeed0fa25ce
chandini vysyaraju <chandini.vysyaraju@anonyfy.io>
2026-05-15 20:03:42 +0200

    314 trainable models from layerwise learning 2nd stratergy set

---
+++ ab/nn/nn/llr2-79ee1d08d178608b50e820a69aa706eb.py
@@ -0,0 +1,69 @@
+import torch
+import torch.nn as nn
+
+
+def supported_hyperparameters():
+    return {'lr', 'momentum', 'dropout'}
+
+
+class Net(nn.Module):
+
+    def train_setup(self, prm):
+        self.to(self.device)
+        self.criteria = (nn.CrossEntropyLoss().to(self.device),)
+        # Layerwise LR strategy: llr2_cyclic_01x (cyclic)
+        _llr_params = list(self.named_parameters())
+        _llr_cycle = [0.1, 1]
+        _llr_buckets = {}
+        for _llr_i, (_, _llr_p) in enumerate(_llr_params):
+            _llr_m = _llr_cycle[_llr_i % 2]
+            _llr_buckets.setdefault(_llr_m, []).append(_llr_p)
+        _llr_groups = [{'params': _llr_ps, 'lr': prm.get('lr', 0.01) * _llr_m} for _llr_m, _llr_ps in _llr_buckets.items()]
+        self.optimizer = torch.optim.SGD(_llr_groups, lr=prm['lr'], momentum=prm['momentum'])
+
+    def learn(self, train_data):
+        for inputs, labels in train_data:
+            inputs, labels = inputs.to(self.device), labels.to(self.device)
+            self.optimizer.zero_grad()
+            outputs = self(inputs)
+            loss = self.criteria[0](outputs, labels)
+            loss.backward()
+            nn.utils.clip_grad_norm_(self.parameters(), 3)
+            self.optimizer.step()
+
+    def __init__(self, in_shape: tuple, out_shape: tuple, prm: dict, device: torch.device) -> None:
+        super().__init__()
+        self.device = device
+        self.features = nn.Sequential(
+            nn.Conv2d(in_shape[1], 64, kernel_size=11, stride=4, padding=2),
+            nn.ReLU(inplace=True),
+            nn.MaxPool2d(kernel_size=3, stride=2),
+            nn.Conv2d(64, 192, kernel_size=5, padding=2),
+            nn.ReLU(inplace=True),
+            nn.MaxPool2d(kernel_size=3, stride=2),
+            nn.Conv2d(192, 384, kernel_size=3, padding=1),
+            nn.ReLU(inplace=True),
+            nn.Conv2d(384, 256, kernel_size=3, padding=1),
+            nn.ReLU(inplace=True),
+            nn.Conv2d(256, 256, kernel_size=3, padding=1),
+            nn.ReLU(inplace=True),
+            nn.MaxPool2d(kernel_size=3, stride=2),
+        )
+        dropout: float = prm['dropout']
+        self.avgpool = nn.AdaptiveAvgPool2d((6, 6))
+        self.classifier = nn.Sequential(
+            nn.Dropout(p=dropout),
+            nn.Linear(256 * 6 * 6, 4096),
+            nn.ReLU(inplace=True),
+            nn.Dropout(p=dropout),
+            nn.Linear(4096, 4096),
+            nn.ReLU(inplace=True),
+            nn.Linear(4096, out_shape[0]),
+        )
+
+    def forward(self, x: torch.Tensor) -> torch.Tensor:
+        x = self.features(x)
+        x = self.avgpool(x)
+        x = torch.flatten(x, 1)
+        x = self.classifier(x)
+        return x
